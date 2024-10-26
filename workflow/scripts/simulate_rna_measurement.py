51e9daeba02de374311be89c2c7ca32bedf259f3
Moshir Harsh <btemoshir@gmail.com>
2024-10-26 12:09:27 +0200

    Added Phosphate weights in the fragments accounting for 'c-Y' breakage with 3' and 5' support. Also added Pyramidine Modifications.

--- workflow/scripts/simulate_rna_measurement.py
+++ workflow/scripts/simulate_rna_measurement.py
@@ -14,13 +14,8 @@ true_sequence = nucleoside_re.findall(snakemake.wildcards.seq)
 n_fragments = int(snakemake.wildcards.n_fragments)
 nucleoside_masses = pl.read_csv(snakemake.input[0], separator="\t")
 max_n_parts = int(snakemake.config["parameters"][0]["max_n_parts"])
-
-# TODO also simulate different tails:
-phosphorus = 30.973761998
-oxygen = 31.989829239
-d_ribose = 150.05282342
-tail_5prime = [d_ribose, oxygen, 2 * oxygen + phosphorus, oxygen]
-
+rel_error_rate = snakemake.config["parameters"][0]["rel_error_rate"]
+backbone_masses = snakemake.config["backbone_weights"][0]
 
 # helper functions
 #def random_fragment():
@@ -30,20 +25,20 @@ tail_5prime = [d_ribose, oxygen, 2 * oxygen + phosphorus, oxygen]
 
 # Divide the sequence into a maximum given number of "max_n_parts". 
 # At high energies the sequence can be divided into multiple parts! Sample uniformly how many parts it gets broken into.
-# TODO: Implement the proper addition of backbone masses with the nucleoside masses
-# TODO: Implement that in some cases there is no base pair generated, but only the backbone with sugar etc.
-
 def random_fragment(num_parts = max_n_parts):
-    if num_parts == 1: #If it breaks into one part, the entire sequence remains intact.
+    if num_parts == 1: 
+        #If it breaks into one part, the entire sequence remains intact.
         breakagepoints = [int(0)]
-    elif num_parts <= len(true_sequence): #If the number of parts is less than or equal to the sequence length, then the sequence gets broken into the number of parts.
+    elif num_parts <= len(true_sequence): 
+        #If the number of parts is less than or equal to the sequence length, then the sequence gets broken into the number of parts.
         breakagepoints = sorted(random.sample(range(1, len(true_sequence)), num_parts - 1))
-    elif num_parts > len(true_sequence): #The number of parts cannot be greater than the sequence length.
+    elif num_parts > len(true_sequence): 
+        #The number of parts cannot be greater than the sequence length.
         raise ValueError("The number of parts cannot be greater than the sequence length!")
     return breakagepoints
+# TODO: Implement that in some cases there is no base pair generated, but only the backbone with sugar etc?
 
-#Select how many parts the sequence gets broken into.
-#It can happen that the entire sequence remains intact.
+#Select how many parts the sequence gets broken into. #It can happen that the entire sequence remains intact.
 breakagepoints = [random_fragment(num_parts = random.randint(1,max_n_parts)) for _ in range(n_fragments)]
 
 # Generate "left" and "right" neuclotides based on the breakagepoints, 
@@ -70,6 +65,14 @@ fragments = pl.from_records(
     orient="row",
 )
 
+# TODO: Implement breaking at other points
+#Middle -> -H + PO2
+#5'end  -> correct
+#3'end  -> +PO2
+backbone_middle_addition = backbone_masses["phosphorus"] + 2*backbone_masses["oxygen"] - backbone_masses["hydrogen"]
+backbone_start_addition  = 0.
+backbone_end_addition    = backbone_masses["phosphorus"] + 2*backbone_masses["oxygen"]
+
 # calculate true masses based on known nucleoside masses
 true_fragment_masses = [
     sum(
@@ -81,20 +84,40 @@ true_fragment_masses = [
     for fragment in fragments.iter_rows(named=True)
 ]
 
-# simulate observed masses with noise
-# TODO include breaking at different tails
-fragment_noise = norm.rvs(scale=0.5, size=len(true_fragment_masses))
+#Copying the fragment masses to a new list to add the backbone masses to the fragments.
+true_fragment_masses_with_backbone = [elem for elem in true_fragment_masses]
+
+# Add backbone masses to the fragments, based on the position of the fragment in the sequence!
+for iter,fragment in enumerate(fragments.iter_rows(named=True)):
+    
+    if fragment["left"] == 0:
+        true_fragment_masses_with_backbone[iter] += backbone_start_addition
+    elif fragment["left"] != len(true_sequence)-1:
+        true_fragment_masses_with_backbone[iter] += backbone_middle_addition
+    
+    if fragment["right"] == len(true_sequence)-1:
+        true_fragment_masses_with_backbone[iter] += backbone_end_addition
+    elif fragment["right"] != 0:
+        true_fragment_masses_with_backbone[iter] += backbone_middle_addition
+    
+    true_fragment_masses_with_backbone[iter] += (
+        backbone_middle_addition*max(fragment["right"]-fragment["left"]+1-2,0))
+    
+    
+# simulate observed masses with noise -- we use the relative error rate to simulate the noise!
+fragment_noise = norm.rvs(scale=rel_error_rate, size=len(true_fragment_masses_with_backbone))
 observed_fragment_masses = [
-    max(true_mass + noise, 0.0)
-    for noise, true_mass in zip(fragment_noise, true_fragment_masses)
+    max(mass + mass*noise, 0.0)
+    for noise, mass in zip(fragment_noise, true_fragment_masses_with_backbone)
 ]
 
 # compile final dataframe
 fragments = fragments.with_columns(
-    (pl.col("left") == 0).alias("is_start"),
-    (pl.col("right") == (len(true_sequence))-1).alias("is_end"),
+    (pl.col("left") == 0).alias("is_start (5')"),
+    (pl.col("right") == (len(true_sequence))-1).alias("is_end (3')"),
     (pl.col("right") == pl.col("left")).alias("single_nucleotide"),
-    pl.Series(true_fragment_masses).alias("true_mass"),
+    pl.Series(true_fragment_masses).alias("true_nucleoside_mass"),
+    pl.Series(true_fragment_masses_with_backbone).alias("true_mass_with_backbone"),
     pl.Series(observed_fragment_masses).alias("observed_mass"),
 )
 
