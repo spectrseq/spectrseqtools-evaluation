1044b6d32e3e898203d16c5f28b938019e84d753
Kühle, Laura Christine (lakue103) <laura.kuehle@uni-duesseldorf.de>
2025-06-10 13:57:08 +0200

    Fixed typos and made sure naming adheres to conventions.

--- workflow/scripts/simulate_rna_measurement.py
+++ workflow/scripts/simulate_rna_measurement.py
@@ -16,12 +16,12 @@ nucleoside_masses = pl.read_csv(snakemake.input[0], separator="\t")
 
 max_n_parts = int(snakemake.config["fragmentation_parameters"][0]["max_n_parts"])
 rel_error_rate = snakemake.config["fragmentation_parameters"][1]["rel_error_rate"]
-breakageline = snakemake.config["fragmentation_parameters"][2]["breakage_line"]
+breakage_line = snakemake.config["fragmentation_parameters"][2]["breakage_line"]
 fragmentation_process = snakemake.config["fragmentation_parameters"][3][
     "fragmentation_process"
 ]
-noise_distrubution = snakemake.config["fragmentation_parameters"][4][
-    "noise_distrubution"
+noise_distribution = snakemake.config["fragmentation_parameters"][4][
+    "noise_distribution"
 ]
 
 
@@ -42,14 +42,14 @@ backbone_masses = {
 def random_fragment(num_parts=max_n_parts):
     if num_parts == 1:
         # If it breaks into one part, the entire sequence remains intact.
-        breakagepoints = [int(0)]
+        breakage_points = [int(0)]
 
     elif num_parts <= len(true_sequence):
         # If the number of parts is less than or equal to the sequence length, then the sequence gets broken into the number of parts.
-        breakagepoints = sorted(
+        breakage_points = sorted(
             random.sample(range(1, len(true_sequence)), num_parts - 1)
         )
-        # Change this 1 to 2 if there are not enough statistics for the sequence brekage!
+        # Change this 1 to 2 if there are not enough statistics for the sequence breakage!
 
     elif num_parts > len(true_sequence):
         # The number of parts cannot be greater than the sequence length.
@@ -57,7 +57,7 @@ def random_fragment(num_parts=max_n_parts):
             "The number of parts cannot be greater than the sequence length!"
         )
 
-    return breakagepoints
+    return breakage_points
 
 
 # TODO: Implement that in some cases there is no base pair generated, but only the backbone with sugar etc?
@@ -67,25 +67,25 @@ if fragmentation_process == "random":
     # At high energies the sequence can be divided into multiple parts! Sample uniformly how many parts it gets broken into.
     # Each fragmentation process (n_fragments) results in (1,max_n_parts) number of fragments (uniformly).
     # It can happen that the entire sequence remains intact.
-    breakagepoints = [
+    breakage_points = [
         random_fragment(num_parts=random.randint(1, max_n_parts))
         for _ in range(n_fragments)
     ]
 elif fragmentation_process == "exact":
     # Each fragmentation process (n_fragments) results in exactly max_n_parts number of fragments.
-    breakagepoints = [
+    breakage_points = [
         random_fragment(num_parts=max_n_parts) for _ in range(n_fragments)
     ]
 
 
-# Generate "left" and "right" neuclotides based on the breakagepoints,
+# Generate "left" and "right" nucleotides based on the breakage points,
 # which are the exact indices of the nucleotides in the generated fragments:
 
 
-def generate_left_right(breakagepoints):
+def generate_left_right(breakage_points):
     left, right = [], []
 
-    for exp in breakagepoints:
+    for exp in breakage_points:
         left.append(0)
 
         for part in exp:
@@ -97,7 +97,7 @@ def generate_left_right(breakagepoints):
     return left, right
 
 
-l_breakage, r_breakage = generate_left_right(breakagepoints)
+l_breakage, r_breakage = generate_left_right(breakage_points)
 
 # sample random fragments from true sequence
 fragments = pl.from_records(
@@ -118,9 +118,9 @@ true_fragment_masses = [
 ]
 
 # Calculate observed masses WITHOUT the backbone masses
-if noise_distrubution == "normal":
+if noise_distribution == "normal":
     fragment_noise = norm.rvs(scale=rel_error_rate, size=len(true_fragment_masses))
-elif noise_distrubution == "uniform":
+elif noise_distribution == "uniform":
     fragment_noise = (
         uniform.rvs(scale=rel_error_rate, size=len(true_fragment_masses))
         - rel_error_rate / 2
@@ -132,11 +132,13 @@ observed_fragment_masses_without_backbone = [
 
 
 # Add backbone masses to the fragments, based on the position of the fragment in the sequence!
-def add_backbone_masses(fragment, backbone_masses, fragment_masses, breakageline="c-y"):
-    if breakageline == "c-y":
+def add_backbone_masses(
+    fragment, backbone_masses, fragment_masses, breakage_line="c-y"
+):
+    if breakage_line == "c-y":
         # TODO: Implement breaking at other points, this is currently c-y breakgae
         # Middle -> + PO2 - H
-        # 5'end (start)  -> correect (-H) (We add an additional H at the end)
+        # 5'end (start)  -> correct (-H) (We add an additional H at the end)
         # 3'end (end) cyclic phosphate -> + PO2  - 2H
         backbone_middle_addition = (
             backbone_masses["phosphorous"]
@@ -150,14 +152,14 @@ def add_backbone_masses(fragment, backbone_masses, fragment_masses, breakageline
             - 2 * backbone_masses["hydrogen"]
         )  # Cyclic phosphate
 
-    elif breakageline == "a-w":
+    elif breakage_line == "a-w":
         raise NotImplementedError("Breaking at 'a-w' is not implemented yet.")
 
-    elif breakageline == "b-x":
-        raise NotImplementedError("Breaking at 'a-w' is not implemented yet.")
+    elif breakage_line == "b-x":
+        raise NotImplementedError("Breaking at 'b-x' is not implemented yet.")
 
-    elif breakageline == "d-z":
-        raise NotImplementedError("Breaking at 'a-w' is not implemented yet.")
+    elif breakage_line == "d-z":
+        raise NotImplementedError("Breaking at 'd-z' is not implemented yet.")
 
     for iter, fragment in enumerate(fragments.iter_rows(named=True)):
         if fragment["left"] == 0:
@@ -193,11 +195,11 @@ true_fragment_masses_with_backbone = add_backbone_masses(
 
 
 # simulate observed masses with noise -- we use the relative error rate to simulate the noise!
-if noise_distrubution == "normal":
+if noise_distribution == "normal":
     fragment_noise = norm.rvs(
         scale=rel_error_rate, size=len(true_fragment_masses_with_backbone)
     )
-elif noise_distrubution == "uniform":
+elif noise_distribution == "uniform":
     fragment_noise = (
         uniform.rvs(scale=rel_error_rate, size=len(true_fragment_masses_with_backbone))
         - rel_error_rate / 2
@@ -217,10 +219,18 @@ fragment_sequences = [
 fragments = fragments.with_columns(
     # (pl.col("left") == 0).alias("is_start"),
     # (pl.col("right") == (len(true_sequence))).alias("is_end"),
-    ((pl.col("left") == 0) & (~(pl.col("right") == (len(true_sequence))))).alias("is_start"),
-    ((pl.col("right") == (len(true_sequence))) & (~(pl.col("left") == 0))).alias("is_end"),
-    ((pl.col("left") == 0) & (pl.col("right") == (len(true_sequence)))).alias("is_start_end"),
-    ((~(pl.col("left") == 0)) & (~(pl.col("right") == (len(true_sequence))))).alias("is_internal"),
+    ((pl.col("left") == 0) & (~(pl.col("right") == (len(true_sequence))))).alias(
+        "is_start"
+    ),
+    ((pl.col("right") == (len(true_sequence))) & (~(pl.col("left") == 0))).alias(
+        "is_end"
+    ),
+    ((pl.col("left") == 0) & (pl.col("right") == (len(true_sequence)))).alias(
+        "is_start_end"
+    ),
+    ((~(pl.col("left") == 0)) & (~(pl.col("right") == (len(true_sequence))))).alias(
+        "is_internal"
+    ),
     (pl.col("right") == pl.col("left") + 1).alias("single_nucleoside"),
     pl.Series(fragment_sequences).alias("sequence"),
     pl.Series(true_fragment_masses).alias("true_nucleoside_mass"),
