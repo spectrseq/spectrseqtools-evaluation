6e32f5c9f8e16a493326e437e39f9b4c78771d24
Kühle, Laura Christine (lakue103) <laura.kuehle@uni-duesseldorf.de>
2025-06-12 15:37:57 +0200

    Adapted config to adhere to standard format.

--- workflow/scripts/simulate_rna_measurement.py
+++ workflow/scripts/simulate_rna_measurement.py
@@ -12,23 +12,21 @@ nucleoside_re = re.compile(r"\d*[ACGU]")
 # input
 true_sequence = nucleoside_re.findall(snakemake.wildcards.seq)
 n_fragments = int(snakemake.wildcards.n_fragments)
-nucleoside_masses = pl.read_csv(snakemake.input[0], separator="\t")
+nucleoside_masses = pl.read_csv(snakemake.input["nucleosides"], separator="\t")
+element_masses = pl.read_csv(snakemake.input["elements"], separator="\t")
 
-max_n_parts = int(snakemake.config["fragmentation_parameters"][0]["max_n_parts"])
-rel_error_rate = snakemake.config["fragmentation_parameters"][1]["rel_error_rate"]
-breakage_line = snakemake.config["fragmentation_parameters"][2]["breakage_line"]
-frag_process = snakemake.config["fragmentation_parameters"][3][
-    "fragmentation_process"
-]
-noise_dist = snakemake.config["fragmentation_parameters"][4][
-    "noise_distribution"
-]
+max_n_parts = int(snakemake.config["fragmentation_params"]["max_n_parts"])
+rel_error_rate = snakemake.config["fragmentation_params"]["rel_error_rate"]
+breakage_line = snakemake.config["fragmentation_params"]["breakage_line"]
+frag_process = snakemake.config["fragmentation_params"]["fragmentation_process"]
+noise_dist = snakemake.config["fragmentation_params"]["noise_distribution"]
+mass_5_prime = snakemake.config["fragmentation_params"]["mass_5_prime"]
+mass_3_prime = snakemake.config["fragmentation_params"]["mass_3_prime"]
 
-
-backbone_masses = snakemake.config["backbone_weights"]
-backbone_masses = {
-    k: v for d in backbone_masses for k, v in d.items()
-}  # Convert the list of dictionaries to a single dictionary.
+# Convert dataframe of element weights to dict
+element_masses = {row[element_masses.get_column_index("symbol")]:
+                  row[element_masses.get_column_index("mass")] for
+                  row in element_masses.iter_rows()}
 
 # helper functions
 # def random_fragment():
@@ -144,24 +142,25 @@ observed_fragment_masses_without_backbone = [
 
 # Add backbone masses to the fragments, based on the position of the fragment in the sequence!
 def add_backbone_masses(
-    fragment, backbone_masses, fragment_masses, breakage_line="c/y"
+    fragment, element_masses, fragment_masses, breakage_line="c/y",
+    mass_5_prime=mass_5_prime, mass_3_prime=mass_3_prime
 ):
     match breakage_line:
         case "c/y":
-            # TODO: Implement breaking at other points, this is currently c/y breakgae
+            # TODO: Implement breaking at other points, this is currently c/y breakage
             # Middle -> + PO2 - H
             # 5'end (start)  -> correct (-H) (We add an additional H at the end)
             # 3'end (end) cyclic phosphate -> + PO2  - 2H
             backbone_middle_addition = (
-                backbone_masses["phosphorous"]
-                + 2 * backbone_masses["oxygen"]
-                - backbone_masses["hydrogen"]
+                element_masses["P"]
+                + 2 * element_masses["O"]
+                - element_masses["H+"]
             )
-            backbone_start_addition = -backbone_masses["hydrogen"]
+            backbone_start_addition = -element_masses["H+"]
             backbone_end_addition = (
-                backbone_masses["phosphorous"]
-                + 2 * backbone_masses["oxygen"]
-                - 2 * backbone_masses["hydrogen"]
+                element_masses["P"]
+                + 2 * element_masses["O"]
+                - 2 * element_masses["H+"]
             )  # Cyclic phosphate
         case "a/w":
             raise NotImplementedError("Breaking at 'a/w' is not implemented yet.")
@@ -177,7 +176,7 @@ def add_backbone_masses(
     for iter, fragment in enumerate(fragments.iter_rows(named=True)):
         if fragment["left"] == 0:
             fragment_masses[iter] += (
-                backbone_start_addition + backbone_masses["Tag5prime"]
+                backbone_start_addition + mass_5_prime
             )  # Can add a 5'tag mass if applicable here!
         else:
             fragment_masses[iter] += (
@@ -185,9 +184,8 @@ def add_backbone_masses(
             )
 
         if fragment["right"] == len(true_sequence):
-            fragment_masses[iter] += +backbone_masses[
-                "Tag3prime"
-            ]  # Can add a 3'tag mass if applicable here!
+            fragment_masses[iter] += mass_3_prime
+            # Can add a 3'tag mass if applicable here!
             # Note that if the other end of one of the fragment corressponds to the 3' end, then there is no special mass addition to be considered.
 
         fragment_masses[iter] += backbone_middle_addition * max(
@@ -195,7 +193,7 @@ def add_backbone_masses(
         )
 
         # Add a terminal Hydrogen
-        fragment_masses[iter] += backbone_masses["hydrogen"]
+        fragment_masses[iter] += element_masses["H+"]
 
     return fragment_masses
 
@@ -203,7 +201,7 @@ def add_backbone_masses(
 # Copying the fragment masses to a new list to add the backbone masses to the fragments.
 true_fragment_masses_with_backbone = [elem for elem in true_fragment_masses]
 true_fragment_masses_with_backbone = add_backbone_masses(
-    fragments, backbone_masses, true_fragment_masses_with_backbone
+    fragments, element_masses, true_fragment_masses_with_backbone
 )
 
 
